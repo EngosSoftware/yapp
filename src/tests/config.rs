8211d6d08e890e4bc0bee3190c285ed89ca36386
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2024-04-23 15:15:48 +0200

    Updated tests.

--- src/tests/config.rs
+++ src/tests/config.rs
@@ -5,15 +5,21 @@ use crate::config::load_config;
 #[test]
 fn loading_config_should_work() {
   let config = r#"
-  
+
+
   alfa
   beta
+
 
   @version 
   0.1.3
 
+
+
+
+
   "#;
 
   let processor = load_config(config).unwrap();
-  assert_eq!("a beta b 0.1.3 c", processor.replace("a alfa b @version c"));
+  assert_eq!("│ beta │ 0.1.3 │", processor.replace("│ alfa │ @version │"));
 }
